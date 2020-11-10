b36d12417b658c8e4dd46da6258f21ce61f480ea
Yehuda Bortz <yehudabortz@icloud.com>
2020-11-10 11:02:06 -0600

    commit tree 6b57e02ca24f2b5e3863bb49e4e47bd3941fc141

---
+++ lib/song.rb
@@ -0,0 +1,12 @@
+
+class Song
+    attr_accessor :title, :artist
+
+    # def initialize(title) 
+    #     @title = title
+    #   end
+end
+
+# beyonce = Artist.new("Beyonce")
+# seven_eleven = Song.new("7/11")
+# seven_eleven.artist = beyonce
